a9e86374e765049a6684524b7c34b6b831afe240
devansh19jain <devanshjain1910@gmail.com>
2023-12-07 04:53:51 +0530

    Test4

--- backend/routes/getUser.js
+++ backend/routes/getUser.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 //const {isAdmin, isAuthenticated, isSignedIn} = require("../controllers/auth");
 
-router.post("/booking", userController.findUserByEmail);
+router.post("/getUser", userController.findUserByEmail);
 
 module.exports = router;
